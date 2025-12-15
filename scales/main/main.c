5670abbd1d28cf3a9f54ef7418903059b1818711
Truep <pvorman54@gmail.com>
2025-12-16 02:07:21 +0700

    scales wifi fix

--- scales/main/main.c
+++ scales/main/main.c
@@ -1,6 +1,7 @@
 #include "driver/gpio.h"
 #include "esp_event.h"
 #include "esp_log.h"
+#include "esp_mac.h"
 #include "esp_system.h"
 #include "esp_vfs_dev.h"
 #include "esp_wifi.h"
@@ -15,7 +16,7 @@
 #define WIFI_SSID "BLESSING"
 #define WIFI_PASS "nice cock!"
 #define WIFI_CONNECTED_BIT BIT0
-#define WIFI_FAIL_BIT      BIT1
+#define WIFI_FAIL_BIT BIT1
 static EventGroupHandle_t s_wifi_event_group;
 static const char *WIFI_TAG = "WIFI";
 
@@ -152,23 +153,29 @@ static void mqtt_event_handler(void *handler_args, esp_event_base_t base,
   }
 }
 
-static void wifi_event_handler(void* arg, esp_event_base_t event_base,
-                               int32_t event_id, void* event_data) {
+static void wifi_event_handler(void *arg, esp_event_base_t event_base,
+                               int32_t event_id, void *event_data) {
   if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_START) {
     esp_wifi_connect();
-  } else if (event_base == WIFI_EVENT && event_id == WIFI_EVENT_STA_DISCONNECTED) {
+  } else if (event_base == WIFI_EVENT &&
+             event_id == WIFI_EVENT_STA_DISCONNECTED) {
     xEventGroupClearBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
     xEventGroupSetBits(s_wifi_event_group, WIFI_FAIL_BIT);
     ESP_LOGI(WIFI_TAG, "Failed to connect to WiFi, retrying...");
     esp_wifi_connect();
   } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-    ip_event_got_ip_t* event = (ip_event_got_ip_t*) event_data;
+    ip_event_got_ip_t *event = (ip_event_got_ip_t *)event_data;
     ESP_LOGI(WIFI_TAG, "Got IP: " IPSTR, IP2STR(&event->ip_info.ip));
     xEventGroupSetBits(s_wifi_event_group, WIFI_CONNECTED_BIT);
   }
 }
 
 void wifi_connect_simple() {
+  s_wifi_event_group = xEventGroupCreate();
+  if (s_wifi_event_group == NULL) {
+    ESP_LOGE(WIFI_TAG, "Failed to create event group");
+    return;
+  }
   ESP_ERROR_CHECK(esp_netif_init());
   ESP_ERROR_CHECK(esp_event_loop_create_default());
   esp_netif_create_default_wifi_sta();
@@ -176,6 +183,11 @@ void wifi_connect_simple() {
   wifi_init_config_t cfg = WIFI_INIT_CONFIG_DEFAULT();
   cfg.nvs_enable = false;
   ESP_ERROR_CHECK(esp_wifi_init(&cfg));
+
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      WIFI_EVENT, ESP_EVENT_ANY_ID, &wifi_event_handler, NULL, NULL));
+  ESP_ERROR_CHECK(esp_event_handler_instance_register(
+      IP_EVENT, IP_EVENT_STA_GOT_IP, &wifi_event_handler, NULL, NULL));
 
   wifi_config_t wifi_config = {
       .sta =
@@ -193,19 +205,17 @@ void wifi_connect_simple() {
   ESP_ERROR_CHECK(esp_wifi_connect());
 
   ESP_LOGI(WIFI_TAG, "Connecting to WiFi...");
-    
-    // Ждем подключения с таймаутом
-    EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
-            WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
-            pdFALSE,
-            pdFALSE,
-            portMAX_DELAY);
 
-    if (bits & WIFI_CONNECTED_BIT) {
-        ESP_LOGI(WIFI_TAG, "Successfully connected to WiFi");
-    } else if (bits & WIFI_FAIL_BIT) {
-        ESP_LOGE(WIFI_TAG, "Failed to connect to WiFi");
-    }
+  // Ждем подключения с таймаутом
+  EventBits_t bits = xEventGroupWaitBits(s_wifi_event_group,
+                                         WIFI_CONNECTED_BIT | WIFI_FAIL_BIT,
+                                         pdFALSE, pdFALSE, portMAX_DELAY);
+
+  if (bits & WIFI_CONNECTED_BIT) {
+    ESP_LOGI(WIFI_TAG, "Successfully connected to WiFi");
+  } else if (bits & WIFI_FAIL_BIT) {
+    ESP_LOGE(WIFI_TAG, "Failed to connect to WiFi");
+  }
 }
 
 void app_main() {
